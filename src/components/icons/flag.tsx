ae4eb0aabe824b65c9905f15c61a81585db47dbd
chisomdaniel <chinwezechisom@gmail.com>
2026-08-19 09:12:31 +0100

    fixed some issues with broken images

--- src/components/icons/flag.tsx
+++ src/components/icons/flag.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/utils/cn";
-import { useState } from "react";
 
 export default function Flags({
   countryCode,
@@ -12,17 +11,16 @@ export default function Flags({
   className?: string;
   size?: string;
 }) {
-  const [hasError, setHasError] = useState(false);
-
   const fallbackUrl =
     "https://purecatamphetamine.github.io/country-flag-icons/3x2/XO.svg";
+  const addDefaultImg = (ev: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    ev.currentTarget.src = fallbackUrl;
+    ev.currentTarget?.classList.add("grayscale");
+    ev.currentTarget.onerror = null; // prevents looping
+  };
   return (
     <img
-      src={
-        hasError
-          ? fallbackUrl
-          : `https://purecatamphetamine.github.io/country-flag-icons/3x2/${countryCode.slice(0, 2).toUpperCase()}.svg`
-      }
+      src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${countryCode.slice(0, 2).toUpperCase()}.svg`}
       alt={`${alt} flag`}
       className={cn(
         "shrink-0 w-6 h-6 rounded-full overflow-hidden object-cover",
@@ -31,7 +29,7 @@ export default function Flags({
       width={size}
       height={size}
       loading="lazy"
-      onError={() => setHasError(true)}
+      onError={addDefaultImg}
     />
   );
 }
